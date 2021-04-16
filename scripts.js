ac7c23643cb58bac9dab1f4553178207629becec
alexseymourdev <alexseymourdev@gmail.com>
2021-04-16 14:19:31 +0100

    adding function to process slots on click

--- scripts.js
+++ scripts.js
@@ -1,10 +1,25 @@
 objConnectFour = {
     init:function(){
         this.getAllSlots();
+        this.addEventListeners();
     },
     getAllSlots:function(){
-        this.objSlots = document.querySelectorAll('.checker');
-        console.log(this.objSlots);
+        this.arrSlots = document.querySelectorAll('.checker');
+        console.log(this.arrSlots);
+    },
+    addEventListeners:function(){
+        _self = this;
+        for(counter = 0; counter < this.arrSlots.length; counter++){
+            // console.log(arrNumbers[counter]);
+            objSlot = this.arrSlots[counter];
+            objSlot.addEventListener("click", function(event){
+                currentItem = event.target;
+                _self.processSlot(currentItem);
+            });
+        }
+    },
+    processSlot(currentItem){
+        console.log(currentItem);
     }
 }
 objConnectFour.init();
